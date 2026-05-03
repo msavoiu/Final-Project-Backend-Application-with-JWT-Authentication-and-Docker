9af8c66d704afe3ab399b9cf1157af7e6cabb3c6
Madeline Savoiu <savoiu@csu.fullerton.edu>
2026-05-03 15:22:42 -0700

    added getters and setters

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -59,4 +59,61 @@ public class User {
         fetch = FetchType.EAGER
     )
     private List<CustomerOrder> customerOrders = new ArrayList<>();
+
+
+    // getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() { 
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<CustomerOrder> getCustomerOrders() {
+        return customerOrders; 
+    }
+
+    public void setCustomerOrders(List<CustomerOrder> customerOrders) {
+        this.customerOrders = customerOrders;
+    }
 }
